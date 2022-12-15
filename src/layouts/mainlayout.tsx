33aa932d235bb08934ba2558fabd91031afae3d9
MSAZ89 <michaelsimonaz@gmail.com>
2022-12-15 16:24:48 -0700

    clear all button and people list

--- src/layouts/mainlayout.tsx
+++ src/layouts/mainlayout.tsx
@@ -4,7 +4,7 @@ export default function MainLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="p-4 bg-slate-100 rounded max-w-4xl mx-auto m-4">
+    <div className="p-4 bg-slate-100 rounded max-w-4xl mx-auto m-4 my-8">
       {children}
     </div>
   );
